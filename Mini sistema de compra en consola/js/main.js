45578e4d3cb673ce1fbc697e1b12a90b4b1a6719
Sofi11017 <sof71101@gmail.com>
2026-06-15 19:09:49 -0500

    Add files via upload

---
+++ Mini sistema de compra en consola/js/main.js
@@ -0,0 +1,65 @@
+let nombreProducto = " teclado gamer ";
+let precio = 120000;
+let cantidad = 2;
+let descuento = 10000;
+let disponible = true;
+
+let nombreLimpio = nombreProducto.trim();
+let nombreMayusculas = nombreLimpio.toUpperCase();
+
+let contieneGamer = nombreLimpio.toLowerCase().includes("gamer");
+
+function calcularSubtotal(precio, cantidad) {
+    return precio * cantidad;
+}
+
+function calcularTotal(subtotal, descuento) {
+    return subtotal - descuento;
+}
+
+let subtotal = calcularSubtotal(precio, cantidad);
+let total = calcularTotal(subtotal, descuento);
+
+let categorias = [
+    "Periféricos",
+    "Gaming",
+    "Tecnología"
+];
+
+console.log("Primera categoría:", categorias[0]);
+console.log("Cantidad de categorías:", categorias.length);
+
+let producto = {
+    nombre: nombreLimpio,
+    precio: precio,
+    cantidad: cantidad,
+    disponible: disponible,
+    categorias: categorias
+};
+
+console.log("Objeto producto:");
+console.log(producto);
+
+console.log("Nombre:", producto.nombre);
+console.log("Precio:", producto.precio);
+console.log("Disponible:", producto.disponible);
+
+let envioGratis = total >= 200000 && disponible;
+
+if (envioGratis) {
+    console.log("Aplica envío gratis");
+} else {
+    console.log("No aplica envío gratis");
+}
+
+console.log("================================");
+console.log("RESUMEN DE COMPRA");
+console.log("================================");
+console.log("Producto:", nombreMayusculas);
+console.log("Precio unitario:", precio);
+console.log("Cantidad:", cantidad);
+console.log("Subtotal:", subtotal);
+console.log("Descuento:", descuento);
+console.log("Total a pagar:", total);
+console.log("Producto disponible:", disponible);
+console.log("Contiene palabra clave gamer:", contieneGamer);
